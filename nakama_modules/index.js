ad4fd728160aec20480750ef1ed1b3ad59e40159
Mrudula Kotkar <mrudulakotkar@gmail.com>
2026-04-22 23:47:54 +0530

    fix: load nakama

---
+++ nakama_modules/index.js
@@ -0,0 +1,13 @@
+function createMatch(ctx, logger, nk, payload) {
+  const matchId = nk.matchCreate("tictactoe_match", {});
+
+  return JSON.stringify({
+    matchId: matchId
+  });
+}
+
+function InitModule(ctx, logger, nk, initializer) {
+  logger.info("🔥 Nakama module loaded");
+
+  initializer.registerRpc("create_tictactoe_match", createMatch);
+}
