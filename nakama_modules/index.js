feadd3e22acfaaa476b9b011e05c5bde8481f2db
Mrudula Kotkar <mrudulakotkar@gmail.com>
2026-04-23 00:29:46 +0530

    Final Changes

--- nakama_modules/index.js
+++ nakama_modules/index.js
@@ -1,12 +1,14 @@
 function createMatch(ctx, logger, nk, payload) {
+    logger.info("RPC called");
+
     const matchId = nk.matchCreate("tictactoe_match", {});
-    return JSON.stringify({ matchId: matchId });
+    return JSON.stringify({ matchId });
 }
 
 function InitModule(ctx, logger, nk, initializer) {
     logger.info("🔥 Nakama module loaded");
+
     initializer.registerRpc("create_tictactoe_match", createMatch);
 }
 
-// 🔥 THIS LINE IS MISSING IN YOUR CODE
 !InitModule;
