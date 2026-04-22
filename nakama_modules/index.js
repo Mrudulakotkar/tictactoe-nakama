35208d67d69e7d6212f76107666efb18eb9577e1
Mrudula Kotkar <mrudulakotkar@gmail.com>
2026-04-23 00:16:30 +0530

    fix: register RPC properly

--- nakama_modules/index.js
+++ nakama_modules/index.js
@@ -1,13 +1,12 @@
 function createMatch(ctx, logger, nk, payload) {
-  const matchId = nk.matchCreate("tictactoe_match", {});
-
-  return JSON.stringify({
-    matchId: matchId
-  });
+    const matchId = nk.matchCreate("tictactoe_match", {});
+    return JSON.stringify({ matchId: matchId });
 }
 
 function InitModule(ctx, logger, nk, initializer) {
-  logger.info("🔥 Nakama module loaded");
-
-  initializer.registerRpc("create_tictactoe_match", createMatch);
+    logger.info("🔥 Nakama module loaded");
+    initializer.registerRpc("create_tictactoe_match", createMatch);
 }
+
+// 🔥 THIS LINE IS MISSING IN YOUR CODE
+!InitModule;
