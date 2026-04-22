1cfc9cd44d95f5a395073a22d092324047eb857f
Mrudula Kotkar <mrudulakotkar@gmail.com>
2026-04-23 02:28:33 +0530

    Final Changes

--- nakama_modules/index.js
+++ nakama_modules/index.js
@@ -1,14 +1,15 @@
 function createMatch(ctx, logger, nk, payload) {
-    logger.info("RPC called");
+  logger.info("Creating match...");
 
-    const matchId = nk.matchCreate("tictactoe_match", {});
-    return JSON.stringify({ matchId });
+  const matchId = nk.matchCreate("tictactoe_match", {});
+
+  return JSON.stringify({
+    matchId: matchId
+  });
 }
 
 function InitModule(ctx, logger, nk, initializer) {
-    logger.info("🔥 Nakama module loaded");
+  logger.info("🔥 Nakama module loaded");
 
-    initializer.registerRpc("create_tictactoe_match", createMatch);
+  initializer.registerRpc("create_tictactoe_match", createMatch);
 }
-
-!InitModule;
